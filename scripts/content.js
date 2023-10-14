8f73c9b7815f67d0a888db4f5532d34c83f50ed7
LuisL123 <127363604+LuisL123@users.noreply.github.com>
2023-10-14 01:08:30 -0400

    basic setup for superscripts

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,26 @@
-alert("luis liu li")
+// Function to highlight superscript elements
+function highlightSuperscripts() {
+    // Select all <sup> elements
+    const supElements = document.querySelectorAll('sup');
+
+    // Highlight each <sup> element
+    for (let elem of supElements) {
+        elem.style.color = 'blue';
+        elem.style.backgroundColor = 'yellow'; // Added background color to highlight
+    }
+
+    // Select all elements with CSS style `vertical-align: super;`
+    const styledSuperscripts = Array.from(document.querySelectorAll('*')).filter(elem => {
+        const computedStyle = window.getComputedStyle(elem);
+        return computedStyle.verticalAlign === 'super';
+    });
+
+    // Highlight each styled superscript element
+    for (let elem of styledSuperscripts) {
+        elem.style.color = 'blue';
+        elem.style.backgroundColor = 'yellow'; // Added background color to enhance highlighting
+    }
+}
+
+// Run the highlight function
+highlightSuperscripts();
